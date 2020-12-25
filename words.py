8d6f451ed96de7ce1fef4eb050c146391fd6b7a4
Liz <lizbaconjones@gmail.com>
2020-12-25 11:22:58 -0600

    Intial commit. Began building wordlist.

---
+++ words.py
@@ -0,0 +1,14 @@
+# import packages
+import numpy as np
+
+# Start project by creating word lists
+
+# Shakespear 113 Sonnet
+shakespear =  ['Thou', 'art','as','tyrannous','so','as','thou','art',
+'as','those','whose','beauties','proudly','make','them','cruel',
+'for','well','thou','know','to','my','dear','doting','heart',
+'thou','art','the','fairest','and','most','precious','jewel']
+
+# print out word list 
+for i in shakespear:
+    print(i)
