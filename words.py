98678846063e11f7f5d0e3c16263159ab1a1b166
Liz <lizbaconjones@gmail.com>
2020-12-26 22:20:35 -0600

    Built three word lists and used random function to shuffle words in list.

--- words.py
+++ words.py
@@ -1,5 +1,6 @@
 # import packages
 import numpy as np
+import random
 
 # Start project by creating word lists
 
@@ -9,6 +10,27 @@ shakespear =  ['Thou', 'art','as','tyrannous','so','as','thou','art',
 'for','well','thou','know','to','my','dear','doting','heart',
 'thou','art','the','fairest','and','most','precious','jewel']
 
+# sentence from Courage to Be
+tillich = ['what','is','the','courage','to','be','in','a','situation', 
+'where', 'the', 'existenialist', 'point', 'of', 'view', 'has',
+ 'not', 'burst', 'the', 'essentialist', 'framework']
+
+ # space and time
+space = ['theory','of','relativity','impossible','to','visulize']
+
 # print out word list 
-for i in shakespear:
-    print(i)
+#for i in space:
+    #print(i)
+
+# generate new list with list elements shuffled.
+random.shuffle(space)
+random.shuffle(tillich)
+random.shuffle(shakespear)
+print(space)
+print(tillich)
+print(shakespear)
+import random
+
+l = [0, 1, 2, 3, 4]
+
+#print(random.choice(l))
